322f32d1266050ff6b70a37bc1a7f973a65d4ebb
codezero <gentlekdh@naver.com>
2018-07-08 18:06:48 +0900

    release 18.07.08.01 과제처리를 위한 요구사항 접수 및 환경 구성 최초 maven 프로젝트 생성

---
+++ kakaobank-java-example/src/main/java/kakaobank/core/s1/main.java
@@ -0,0 +1,8 @@
+package kakaobank.core.s1;
+
+public class main {
+	public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+}
