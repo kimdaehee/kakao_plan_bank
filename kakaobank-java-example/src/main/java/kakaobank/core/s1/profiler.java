4e38b42b58da7f1eead6694bb4756c1a9e129882
codezero <gentlekdh@naver.com>
2018-07-14 15:27:48 +0900

    main 변경

--- kakaobank-java-example/src/main/java/kakaobank/core/s1/profiler.java
+++ kakaobank-java-example/src/main/java/kakaobank/core/s1/profiler.java
@@ -17,7 +17,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class main {
+public class profiler {
 	public static Connection connection(String dbFileName) throws ClassNotFoundException, SQLException
     {
             Class.forName("org.sqlite.JDBC");
